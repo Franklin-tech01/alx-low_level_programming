ecd43c14372edc1049f747ef3214e4090edcce4e
Franklin-tech01 <franklinmike2021@gmail.com>
2022-09-10 02:47:15 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ else if (x == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 4 and not 0\n", n, x);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 }
 return (0);
 }
