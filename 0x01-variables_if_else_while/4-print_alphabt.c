6f85e344bcfd9c6c9c1a3ef108046d7231d3b2b6
Franklin-tech01 <franklinmike2021@gmail.com>
2022-09-11 01:55:06 +0100

    e && q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints "all alphabets except e & q"
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+
