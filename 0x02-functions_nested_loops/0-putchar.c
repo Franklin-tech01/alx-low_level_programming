582cd773eeff75ad443a25a201a034195ab66f9d
Franklin-tech01 <franklinmike2021@gmail.com>
2022-09-13 21:10:52 +0100

    this program prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using main function this program prints"putchar"
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+
+	printf("_putchat\n");
+
+	return (0);
+}
