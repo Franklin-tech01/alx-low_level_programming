02243bf2a759c1331ee510e7058eb174f98af3e0
Franklin-tech01 <franklinmike2021@gmail.com>
2022-10-26 13:41:39 -0700

    Linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,8 +10,9 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *
  */
-typedef struct listnt_s
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
